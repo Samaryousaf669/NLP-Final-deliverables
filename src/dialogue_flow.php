86203a1a08c313e580e4505469cc888128bb4065
Samaryousaf669 <bc210415622syo@vu.edu.pk>
2025-07-07 23:19:59 +0500

    Delete src/dialogue_flow.php

--- src/dialogue_flow.php
+++
@@ -1,7 +0,0 @@
-<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-<df-messenger
-  intent="WELCOME"
-  chat-title="LuxBiteChatbot"
-  agent-id="c011bd5a-50f7-4769-b3eb-0d78d7e58f61"
-  language-code="en"
-></df-messenger>
